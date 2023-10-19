4aff9d488e44833d9df4557c1803d231cd9e0843
cunha <luiiz.cunha@hotmail.com>
2023-10-19 16:56:29 -0300

    a

--- src/entities/Image.ts
+++ src/entities/Image.ts
@@ -15,7 +15,7 @@ export class Image {
 
     base64?: string;
 
-    @ManyToOne(type => Model, model => model.images) 
+    @ManyToOne(type => Model, model => model.images, { cascade: true }) 
     @JoinColumn({ name: "model_id" }) 
     model: Model;
 
