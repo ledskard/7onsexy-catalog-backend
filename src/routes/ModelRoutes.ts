2475996499bd8444d45ce16bed14dc28b6ed4d4f
cunha <luiiz.cunha@hotmail.com>
2023-11-01 13:06:47 -0300

    a

--- src/routes/ModelRoutes.ts
+++ src/routes/ModelRoutes.ts
@@ -9,7 +9,7 @@ const authMiddleware = new AuthMiddleware();
 
 modelRouter.post("/", authMiddleware.auth, validateCreateModel, modelController.create);
 modelRouter.get("/:id", modelController.findById);
-modelRouter.put("/:id", modelController.update);
+modelRouter.put("/:id", authMiddleware.auth, modelController.update);
 modelRouter.get("/", modelController.findAll);
 modelRouter.post("/:id", modelController.increaseLike);
 modelRouter.delete("/:id", modelController.delete);
