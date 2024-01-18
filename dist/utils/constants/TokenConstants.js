1c0bf728f50efbbf18efa3fa233c6e5fe192db06
Luiz <luiiz.cunha@hotmail.com>
2024-01-18 12:36:06 -0300

    a

--- dist/utils/constants/TokenConstants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv");
-var TOKEN = {
-    EXPIRE_TOKEN_TIME: process.env.EXPIRE_TOKEN_MINUTES || "1d",
-    SECRET_TOKEN: process.env.SECRET_TOKEN || "secret",
-};
-exports.default = TOKEN;
-//# sourceMappingURL=TokenConstants.js.map
