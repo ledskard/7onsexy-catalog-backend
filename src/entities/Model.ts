c82345e13af6950e66dead152bea97eb459e3e30
Luiz <luiiz.cunha@hotmail.com>
2024-01-19 16:38:15 -0300

    z

--- src/entities/Model.ts
+++ src/entities/Model.ts
@@ -13,7 +13,7 @@ export class Model {
     @Column({ name:"username", unique:true })
     username: string;
     
-    @Column({ name:"email", unique:true })
+    @Column({ name:"email", nullable: true})
     email: string;
 
     @Column({ name:"instagram", nullable: true })
--- src/migrations/1705692872045-adding-email.ts
+++ src/migrations/1705692872045-adding-email.ts
@@ -4,13 +4,11 @@ export class AddingEmail1705692872045 implements MigrationInterface {
     name = 'AddingEmail1705692872045'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`model\` ADD \`email\` varchar(255) NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`model\` ADD UNIQUE INDEX \`IDX_0049b8eed7227e82c98220525f\` (\`email\`)`);
+        await queryRunner.query(`ALTER TABLE \`model\` ADD \`email\` varchar(255) NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`model\` DROP INDEX \`IDX_0049b8eed7227e82c98220525f\``);
-        await queryRunner.query(`ALTER TABLE \`model\` DROP COLUMN \`email\``);
     }
 
 }
