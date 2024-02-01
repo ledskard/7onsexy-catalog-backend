86c7525043e46b6f52d7054b1fe76bb73d6050ad
Luiz <luiiz.cunha@hotmail.com>
2024-02-01 18:02:19 -0300

    as

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ AppDataSource.initialize()
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '30mb'}));
+app.use(express.json({limit: '40mb'}));
 app.use(routes);
 
 const port = process.env.PORT || 8080;
