98d3daa55b113eb00fa43070aa426a1e9d703712
Luiz <luiiz.cunha@hotmail.com>
2024-02-03 22:04:15 -0300

    a

--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -73,9 +73,9 @@ export default class ImageService {
 
   public async deleteFromS3(image: IDeleteImageDTO): Promise<void> { 
     
-    await this.imageRepository.deleteById(image.id);
+    image.id && await this.imageRepository.deleteById(image.id);
     const params = {
-      Bucket: "xbio",
+      Bucket: "7onsexycatalogo",
       Key: image.name,
     };
     try {
