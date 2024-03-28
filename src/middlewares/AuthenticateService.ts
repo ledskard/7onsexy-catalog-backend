5d49f69d25576dba2169824fa8452ea5ba85c049
Luiz <luiiz.cunha@hotmail.com>
2024-03-28 10:36:16 -0300

    a

--- src/middlewares/AuthenticateService.ts
+++ src/middlewares/AuthenticateService.ts
@@ -13,10 +13,10 @@ export default class AuthenticateService {
 
     public async authenticate(username: string, password: string): Promise<User> {
         const user = await this.userRepository.findByUsername(username);
-        // if (!user) throw { status: ErrorStatus.bad_request, message: ErrorMessage.user_or_password_incorrect };
-        // const validPassword = await bcrypt.compare(password, user.password);
-        // if (!validPassword) throw { status: 400, message: ErrorMessage.password_invalid };
-        // deletePassword(user);
+        if (!user) throw { status: ErrorStatus.bad_request, message: ErrorMessage.user_or_password_incorrect };
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) throw { status: 400, message: ErrorMessage.password_invalid };
+        deletePassword(user);
         return user;
     }
  
